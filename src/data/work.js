2da00c9a8662b7a2a4b156e767303afa66432462
ajmal <ajmalajju313786@gmail.com>
2026-09-02 13:56:41 +0530

    Add new project assets and update work data: include 4th Revolution Media, GymBro, Andolan News, Ai Insights, Marathwada News, and Camorent in the project showcase with corresponding thumbnails.

--- src/data/work.js
+++ src/data/work.js
@@ -36,6 +36,7 @@ export const SOCIAL_WORK = [
 ];
 
 export const WEBSITE_WORK = [
+  {id: 'w5', title: '4th Revolution Media', thumbnail: '/assets/Project/4threvolutionmedia.webp', url: 'https://www.4threvolutionmedia.com/' },
   { id: 'w1', title: 'Blckole', thumbnail: '/assets/Project/blckole.webp', url: 'https://www.blckole.com/' },
   { id: 'w2', title: 'Kamat Annaleela', thumbnail: '/assets/Project/kamatannaleela.webp', url: 'https://www.kamatannaleela.com/' },
   { id: 'w3', title: 'Sourberry', thumbnail: '/assets/Project/sourberry.webp', url: 'https://www.sourberryentertainment.com/' },
@@ -48,6 +49,12 @@ export const WEBSITE_WORK = [
 export const APP_WORK = [
   { id: 'a1', title: 'Golt', thumbnail: '/assets/Project/golt.webp', url: 'https://play.google.com/store/apps/details?id=com.ajmalca.client' },
   { id: 'a2', title: 'Expenza', thumbnail: '/assets/Project/expenza.webp', url: 'https://play.google.com/store/apps/details?id=com.mohit29kr.expenza' },
+  { id: 'a3', title: 'GymBro', thumbnail: '/assets/Project/gymbro.webp', url: 'https://play.google.com/store/apps/details?id=com.binarybite.gymbro' },
+  { id: 'a4', title: 'Andolan News', thumbnail: '/assets/Project/andolan-news.webp', url: 'https://play.google.com/store/apps/details?id=com.andolan.news.giantwheel' },
+  { id: 'a5', title: 'Ai Insights', thumbnail: '/assets/Project/ai-insights.webp', url: 'https://play.google.com/store/apps/details?id=com.ai.insights' },
+  { id: 'a6', title: 'Marathwada News', thumbnail: '/assets/Project/marathwada-news.webp', url: 'https://play.google.com/store/apps/details?id=com.marathwada.news' },
+  { id: 'a7', title: 'Camorent', thumbnail: '/assets/Project/camorent_app.webp', url: 'https://apps.apple.com/in/app/camorent-shoots-in-minutes/id6761368659' },
+
 ];
 
 export const VIDEO_WORK = [
